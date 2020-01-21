9a5130dfb0fe8522403753c3556f31bf41a9b32c
Daniel Koublachvili <dannykoubz@gmail.com>
2020-01-22 00:24:45 +1100

    added routes folder

--- server.js
+++ server.js
@@ -13,7 +13,8 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 // Add routes, both API and view
-app.use(routes);
+// app.use(routes);
+require("./routes/htmlRoutes")(app);
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
