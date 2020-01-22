4437923955c795edceba679948b26b63bc5103a6
Daniel Koublachvili <dannykoubz@gmail.com>
2020-01-22 18:05:52 +1100

    latest

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
-const mongoose = require("mongoose");
-const routes = require("./routes");
+// const mongoose = require("mongoose");
+// const routes = require("./routes");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -28,7 +28,7 @@ app.get('*', (req, res) => {
 // require("../routes/htmlRoutes")(app);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
 
 // Start the API server
 app.listen(PORT, function() {
